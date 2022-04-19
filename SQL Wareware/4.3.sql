6748a148fc1f315887f773592fbb2d552b69be37
Rass22 <100615564+Rass22@users.noreply.github.com>
2022-04-19 00:00:23 -0500

    Update 4.3.sql

--- SQL Wareware/4.3.sql
+++ SQL Wareware/4.3.sql
@@ -1,2 +1,3 @@
-SELECT users.name, users.handle, users.email, COUNT AS count_skills FROM (select user_id, count(user_id) from user_skills group by user_id
-having count(*)>0) as mytable inner join users on users.id = mytable.user_id And users.enabled = 't' ORDER BY COUNT, id limit 10;
+select users.name, users.handle, users.email, count as count_skills FROM (
+select user_id, count(user_id)from user_skills group by user_id having count(*)>0) as mytable inner join users
+on users.id = mytable.user_id And users.enabled = 't' order by count, id limit 10;
